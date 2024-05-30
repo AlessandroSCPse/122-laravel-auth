73a04640806794e1a39b9506af3c41ebd3af823b
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-05-30 11:52:45 +0200

    dashboard improvements

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,6 +18,12 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <div>
+                        Benvenuto {{ $user->name }}.
+                    </div>
+
+                    <div>Ti sei loggato con la mail: {{ $user->email }}</div>
                 </div>
             </div>
         </div>
